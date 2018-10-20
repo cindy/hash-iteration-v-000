47bfb34688dbc5d43f62da8a7387c2e38cf9772c
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-20 15:34:08 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,5 +5,5 @@ birthday_kids = {
 }
 
 def happy_birthday(birthday_kids)
-  birthday_kids.each{ |k, v| puts "Happy Birthday #{key}! You are now #{v} years old!"}
+  birthday_kids.each{ |k, v| puts "Happy Birthday #{k}! You are now #{v} years old!"}
 end
